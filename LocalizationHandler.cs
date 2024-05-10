a5b92ceccccc97275e6f5151efad0ddcf4bbd102
Notest_ <noccqlive@gmail.com>
2024-05-10 15:27:19 -0300

    Add project files.

---
+++ LocalizationHandler.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+
+namespace ItemLibrary
+{
+    public static class LocalizationHandler
+    {
+        public static Dictionary<Locale, Dictionary<string, string>> LanguageDictionaries = [];
+        public static Dictionary<string, string>? CurrentLanguageDictionary;
+
+        #region Enum Overload
+
+        public static void AddKeyTranslationForLocale(Locale locale, LocalizationKeys.Keys key, string translation)
+        {
+            if (!LanguageDictionaries.ContainsKey(locale))
+            {
+                LanguageDictionaries.Add(locale, []);
+            }
+
+            LanguageDictionaries[locale].TryAdd(key.ToString(), translation);
+        }
+       
+        public static void AddKeyTranslationsForLocale(Locale locale,
+            Dictionary<LocalizationKeys.Keys, string> dictionary)
+        {
+            Dictionary<string, string> dictionary2 = new Dictionary<string, string>();
+            foreach (KeyValuePair<LocalizationKeys.Keys, string> kvp in dictionary)
+            {
+                dictionary2.Add(kvp.Key.ToString(), kvp.Value);
+            }
+
+            if (!LanguageDictionaries.ContainsKey(locale))
+            {
+                
+                LanguageDictionaries.Add(locale, dictionary2);
+                return;
+            }
+
+            foreach (KeyValuePair<string, string> kvp in dictionary2)
+            {
+                LanguageDictionaries[locale].TryAdd(kvp.Key, kvp.Value);
+            }
+        }
+
+        public static void SetTranslationForLocale(Locale locale, LocalizationKeys.Keys key, string translation)
+        {
+            if (!LanguageDictionaries.ContainsKey(locale))
+            {
+                LanguageDictionaries.Add(locale, []);
+            }
+
+            if (!LanguageDictionaries[locale].TryAdd(key.ToString(), translation))
+            {
+                LanguageDictionaries[locale][key.ToString()] = translation;
+            }
+        }
+
+        public static void SetKeyTranslationsDictionaryForLocale(Locale locale,
+            Dictionary<LocalizationKeys.Keys, string> dictionary)
+        {
+            Dictionary<string, string> dictionary2 = new Dictionary<string, string>();
+            foreach (KeyValuePair<LocalizationKeys.Keys, string> kvp in dictionary)
+            {
+                dictionary2.Add(kvp.Key.ToString(), kvp.Value);
+            }
+
+            if (!LanguageDictionaries.ContainsKey(locale))
+            {
+                LanguageDictionaries.Add(locale, dictionary2);
+                return;
+            }
+
+            LanguageDictionaries[locale] = dictionary2;
+        }
+
+        #endregion
+
+        #region String Overload
+
+        public static void AddKeyTranslationForLocale(Locale locale, string key, string translation)
+        {
+            if (!LanguageDictionaries.ContainsKey(locale))
+            {
+                LanguageDictionaries.Add(locale, []);
+            }
+
+            LanguageDictionaries[locale].TryAdd(key, translation);
+        }
+
+        public static void AddKeyTranslationsForLocale(Locale locale,
+            Dictionary<string, string> dictionary)
+        {
+            if (!LanguageDictionaries.ContainsKey(locale))
+            {
+
+                LanguageDictionaries.Add(locale, dictionary);
+                return;
+            }
+
+            foreach (KeyValuePair<string, string> kvp in dictionary)
+            {
+                LanguageDictionaries[locale].TryAdd(kvp.Key, kvp.Value);
+            }
+        }
+
+        public static void SetTranslationForLocale(Locale locale, string key, string translation)
+        {
+            if (!LanguageDictionaries.ContainsKey(locale))
+            {
+                LanguageDictionaries.Add(locale, []);
+            }
+
+            if (!LanguageDictionaries[locale].TryAdd(key, translation))
+            {
+                LanguageDictionaries[locale][key] = translation;
+            }
+        }
+
+        public static void SetKeyTranslationsDictionaryForLocale(Locale locale,
+            Dictionary<string, string> dictionary)
+        {
+            if (!LanguageDictionaries.ContainsKey(locale))
+            {
+                LanguageDictionaries.Add(locale, dictionary);
+                return;
+            }
+
+            LanguageDictionaries[locale] = dictionary;
+        }
+
+        #endregion
+
+        public static Locale GetCreateLocale(LocaleIdentifier localeIdentifier)
+        {
+            ILocalesProvider localeProvider = LocalizationSettings.AvailableLocales;
+            Locale locale = localeProvider.GetLocale(localeIdentifier);
+
+            if (!locale)
+            {
+                locale = Locale.CreateLocale(localeIdentifier);
+                localeProvider.AddLocale(locale);
+            }
+
+            return locale;
+        }
+
+        public static Locale GetCreateLocale(CultureInfo cultureInfo)
+        {
+            LocaleIdentifier localeIdentifier = new LocaleIdentifier(cultureInfo);
+            return GetCreateLocale(localeIdentifier);
+        }
+    }
+}
---
+++ Patches/LocalizationKeysPatcher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+using Sirenix.Serialization.Utilities;
+using UnityEngine.Localization.Settings;
+using UnityEngine.UIElements;
+
+namespace ItemLibrary.Patches
+{
+    [HarmonyPatch(typeof(LocalizationKeys))]
+    internal class LocalizationKeysPatcher
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(LocalizationKeys.OnLanguageSwitch))]
+        private static void OnLanguageSwitchPatch()
+        {
+            bool success = LocalizationHandler.LanguageDictionaries.TryGetValue(LocalizationSettings.SelectedLocale, 
+                out Dictionary<string, string> value);
+
+            if (success)
+                LocalizationHandler.CurrentLanguageDictionary = value;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(LocalizationKeys.GetLocalizedString))]
+        private static bool GetLocalizedStringPatch(ref LocalizationKeys.Keys key, ref string __result)
+        {
+            if (LocalizationHandler.CurrentLanguageDictionary != null && LocalizationHandler.CurrentLanguageDictionary.TryGetValue(key.ToString(), out __result))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
